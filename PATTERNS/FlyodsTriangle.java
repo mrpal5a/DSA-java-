53cbf879c574bd06a2644d360b2a171dca4c91b1
mrpal5a <107930441+mrpal5a@users.noreply.github.com>
2025-01-23 00:55:02 +0530

    FlyodsTriangle Done

---
+++ PATTERNS/FlyodsTriangle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class FlyodsTriangle{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many lines do you want to print");
+        // int number = 1;
+        int number = sc.nextInt();
+        for(int line = 1; line <= 4; line++){
+            for(int num = 1; num <= line; num++){
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
