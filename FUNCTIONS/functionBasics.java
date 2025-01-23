6fd3144d7489f840a13efbfe395978b1b38c78c7
mrpal5a <107930441+mrpal5a@users.noreply.github.com>
2025-01-23 21:34:52 +0530

    Added functionBasics file

---
+++ FUNCTIONS/functionBasics.java
@@ -0,0 +1,9 @@
+public class functionBasics{
+    // creating a first functio
+    public static void printHelloWorld(){ // void is the return type of this function
+        System.out.println("Hello World"); // it will print the Hello World
+    }
+    public static void main(String args[]){ // this is the main function, when we runs the program,this function is executed first by default and "main" is the reserved keyword in java
+        printHelloWorld(); // calling the printHelloWorld function
+    }
+}
