de2d79d6eef1b4bb708df21f8e0412fb45584b7a
mrpal5a <107930441+mrpal5a@users.noreply.github.com>
2025-01-24 22:02:00 +0530

    understood function overloading using parameters

---
+++ FUNCTIONS/fun_Overloading_param.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class fun_Overloading_param{
+    public static int sum(int a, int b){
+        return a + b;
+    }
+    public static int sum(int a, int b, int c){
+        return a + b + c;
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int add = sum(5,4,8);
+        System.out.print("Sum is " +add);
+    }
+}
