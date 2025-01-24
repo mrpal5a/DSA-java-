ad1d6e52ef3e39c64a6c55f3dade66d853dd2f5e
mrpal5a <107930441+mrpal5a@users.noreply.github.com>
2025-01-24 19:36:21 +0530

    added swapFunction

---
+++ FUNCTIONS/swapValues.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class swapValues{
+    // swap function
+    public static void swapFunction(int a, int b){ 
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("Values after swapping");
+        System.out.println("Value of a is " + a);
+        System.out.println("value of b is " +b); 
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter value of a ");
+        int a = sc.nextInt();
+        System.out.print("Enter the value of b ");
+        int b = sc.nextInt();
+        swapFunction(a, b);
+    }
+}
